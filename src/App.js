52d3659d5e855500a1e8ce40c1f78288a1563b62
Jordan-Shelton <jordansheltonwork@gmail.com>
2021-07-16 12:41:53 -0500

    got everything to show up on the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+import styled from 'styled-components'
+import './index'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +13,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starWarsData, setStarWarsData] = useState([])
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people').then((res) => {
+      setStarWarsData(res.data)
+    })
+    .catch()
+  },[])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {starWarsData.map(function(element, ixd){
+        return <Character
+        name= {element.name}
+        gender = {element.gender}
+        height = {element.height}
+        eye = {element.eye_color}
+        mass = {element.mass}
+        key = {ixd}
+        />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const Character = props => {
+    const {name, height, eye, gender, mass} = props
+
+return (
+    <div>
+        <styleDiv>
+            <styleTitle>
+                {name}
+            </styleTitle>
+            <styleText>
+                {name} is a {`${gender !== 'n/a' ? gender: 'robot'}`} who is {height}cm tall, {`${gender !== 'female' ? "he" : "she"}`} has {eye} eyes and has a mass of {mass}kg.
+            </styleText>
+        </styleDiv> 
+    </div>
+    )
+}
+const styleTitle = styled.h2`
+font-family: cursive
+color: white
+font-size: 38px
+margin-bottom:0
+padding-top:20px
+`
+const styleText = styled.p`
+color: white
+font-size:10px
+font-family: sans-serif
+font-size:20px
+margin-top:0
+`
+const styleDiv = styled.div``
+
+export default Character;
